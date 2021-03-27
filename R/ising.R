c4694bd2a81ab027fda51c2bb0c3d97091c72892
XiaoqiLu <lx2170@cumc.columbia.edu>
2021-03-27 05:56:18 -0700

    add diff_flipped_hamilton_ising() for fast-computation of energy diff, where only one variable is flipped. Benchmark shows huge improvement in computational efficiency.
    tested

--- R/ising.R
+++ R/ising.R
@@ -1,14 +1,36 @@
 #' Hamiltonian of Generalized Ising Model
 #'
+#' @description
+#' `hamiltonian_ising` is used to compute the energy of given configuration in a
+#' generalized Ising model.
+#'
 #' @param u a binary ({-1, 1}-valued) vector or matrix. If `u` is a matrix, each
 #' row is taken as an input vector.
 #' @param h a vector giving the external field.
 #' @param J a symmetric matrix with zero diagonals giving the interaction.
 #'
-#' @return a number
+#' @details
+#' The generalized Ising model (also known as quadratic exponential binary
+#' distribution) is generalizing the adjacent coupling of Ising model to two-way
+#' interactions. The probability mass function of generalized Ising model is
+#' \deqn{P(u \vert h, J) = \frac{1}{Z(h, J)} e^{- H(u; h, J)},}
+#' where \eqn{H(u; h, J) = - h^T u - \frac{1}{2} u^T J u} is the Hamiltonian.
+#'
+#' @return a vector giving the energy of configuration u
 #' @export
 #'
 #' @examples
+#' hamiltonian_ising(
+#'   u = c(+1, +1),
+#'   h = c(0, 0),
+#'   J = matrix(c(0, -1, -1, 0), 2, 2)
+#' )
+#'
+#' hamiltonian_ising(
+#'   u = matrix(c(-1, +1, +1, -1), 2, 2, byrow = TRUE),
+#'   h = c(2, 3),
+#'   J = matrix(0, 2, 2)
+#' )
 hamiltonian_ising <- function(u, h, J) {
   if (is.vector(u)) {
     return(-as.numeric(t(h) %*% u + t(u) %*% J %*% u / 2))
@@ -18,3 +40,50 @@ hamiltonian_ising <- function(u, h, J) {
     stop("type error: u should be a vector or matrix")
   }
 }
+
+#' Diff in Hamiltonian of Generalized Ising Model by Flipping a Variable
+#'
+#' @description
+#' `diff_flipped_hamilton_ising` is a fast-computation method for energy
+#' difference, used in the case where only one variable is flipped.
+#'
+#' @param j the (column) index of the variable to be flipped over
+#' @inheritParams hamiltonian_ising
+#'
+#' @details
+#' \deqn{\Delta_j E(u \vert h, J) = H(\tilde{u}; h, J) - H(u; h, J),}
+#' where \eqn{\tilde{u}_j = - u_j} and \eqn{\tilde{u}_{-j} = u_{-j}}. It can be
+#' shown that \eqn{\Delta_j E(u \vert h, J) = 2 u_j (h_j + J_{j, -j} u_{-j})}.
+#'
+#' @return a vector giving the energy difference
+#' @export
+#'
+#' @examples
+#' h <- c(2, 3)
+#' J <- matrix(c(0, -1, -1, 0), 2, 2)
+#' u <- c(-1, +1)
+#' diff_flipped_hamiltonian_ising(u, 2, h, J)
+#'
+#' u <- matrix(c(-1, +1, +1, -1), 2, 2, byrow = TRUE)
+#' diff_flipped_hamiltonian_ising(u, 1, h, J)
+diff_flipped_hamiltonian_ising <- function(u, j, h, J) {
+  if (is.vector(u)) u <- matrix(u, nrow = 1)
+  return(
+    as.vector(
+      2 * u[, j, drop = FALSE] *
+        (h[j] + u[, -j, drop = FALSE] %*% J[-j, j, drop = FALSE])
+    )
+  )
+}
+
+# gibbs_ising <- function(u, h, J, n_step = 1) {
+#   if (is.vector(u)) u <- matrix(u, nrow = 1)
+#   energy <- hamiltonian_ising(u, h, J)
+#   for (i_step in 1 : n_step) {
+#     for (j in 1 : col(u)) {
+#       u[j, ] <- - u[j, ] # flip the j-th column
+#       energy_flipped <- hamiltonian_ising(u, h, J)
+#
+#     }
+#   }
+# }
--- tests/testthat/test-ising.R
+++ tests/testthat/test-ising.R
@@ -24,3 +24,24 @@ test_that("Hamiltanion of Ising is correct", {
     c(-1, 1)
   )
 })
+
+test_that("Fast computation of diff in Hamilton (flipping one variable) is correct", {
+  h <- c(2, 3)
+  J <- matrix(c(0, -1, -1, 0), 2, 2)
+
+  u <- c(-1, +1)
+  u_flipped <- c(-1, -1) # j = 2
+  energy_diff <- hamiltonian_ising(u_flipped, h, J) - hamiltonian_ising(u, h, J)
+  expect_equal(
+    diff_flipped_hamiltonian_ising(u, 2, h, J),
+    energy_diff
+  )
+
+  u <- matrix(c(-1, +1, +1, -1), 2, 2, byrow = TRUE)
+  u_flipped <- matrix(c(+1, +1, -1, -1), 2, 2, byrow = TRUE) # j = 1
+  energy_diff <- hamiltonian_ising(u_flipped, h, J) - hamiltonian_ising(u, h, J)
+  expect_equal(
+    diff_flipped_hamiltonian_ising(u, 1, h, J),
+    energy_diff
+  )
+})
